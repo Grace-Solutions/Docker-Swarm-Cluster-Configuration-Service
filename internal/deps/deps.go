9dcb19bcb3bd4ad786b3df2887eb96cb69435af4
Alphaeus Mote <Alphaeus.Mote@gmail.com>
2025-11-20 21:01:20 -0500

    Improve Gluster installation on apt-based systems

--- internal/deps/deps.go
+++ internal/deps/deps.go
@@ -177,7 +177,7 @@ func EnsureGluster(ctx context.Context) error {
 
 	if _, err := exec.LookPath("apt-get"); err == nil {
 		manager = "apt-get"
-		script = "apt-get update && apt-get install -y glusterfs-client"
+		script = "apt-get update && apt-get install -y glusterfs-cli glusterfs-client"
 	} else if _, err := exec.LookPath("dnf"); err == nil {
 		manager = "dnf"
 		script = "dnf install -y glusterfs glusterfs-fuse"
